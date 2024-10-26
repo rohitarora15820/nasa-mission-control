ce34082549cef0303a2398b0a5d6a756b72c4efe
rohitarora15820 <rohitarora15820@gmail.com>
2024-10-27 00:42:10 +0530

    Created Script to Start Both Client & Server

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const path=require('path');
 const cors=require('cors');
 
 const planetsRouter=require('./router/planets/planets.router');
@@ -6,11 +7,19 @@ const planetsRouter=require('./router/planets/planets.router');
 
 const app=express();
 
+
 app.use(cors({
     origin:'http://localhost:3000'
 }))
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..','public')));
+
+
 app.use(planetsRouter);
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '..','public','index.html'));
+});
 
 module.exports=app;
 
