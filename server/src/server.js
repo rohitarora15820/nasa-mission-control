6139fbc42e2cd60541df50bc4e7383e60602179d
rohitarora15820 <rohitarora15820@gmail.com>
2024-11-17 14:03:10 +0530

    Setup mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,24 @@
 const http=require('http');
-
+const mongoose=require('mongoose');
 const app=require('./app');
 const {loadPlanetsData}=require('./models/planets.model');
 const PORT=process.env.PORT || 8000;
 
+const MONGO_URL="mongodb+srv://nasa_user:PNI9MvVKpTjXmzPE@nasacluster.rfh6x.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
+
 const server=http.createServer(app);
 
+mongoose.connection.once('open',()=>{
+    console.log('MongoDB Ready!');
+});
+mongoose.connection.once('error',(err)=>{
+    console.log(err);
+ 
+});
 
 async function startServer(){
+
+   await mongoose.connect(MONGO_URL)
 await loadPlanetsData();
 
     server.listen(PORT,()=>{
